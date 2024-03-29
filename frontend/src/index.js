e5c541b88d4fce40481213df0ec20005ea2753f4
Maurice <55408918+Maurice037@users.noreply.github.com>
2024-03-29 23:20:08 +0100

    create React app with react-router

---
+++ frontend/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();

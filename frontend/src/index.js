fe5f75e53922c3957a151ab25193284f193864b1
Maurice <55408918+Maurice037@users.noreply.github.com>
2024-03-31 16:35:13 +0200

    add dark and light mode over system settings
    example code into home.jsx

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Home from './pages/Home';
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,51 @@
 import React from "react";
 import Localize from "../components/localize";
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function Home() {
     return (
-        <Container className="p-3">
-            <Container className="p-5 mb-4 bg-light rounded-3">
-                <h1 className="header"><Localize str="STR_Header" /></h1>
-                <p><Localize str="STR_Text" /></p>
-            </Container>
+        <Container className="p-3 d-flex flex-column align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+            <h1 className="text-center text-skycave mb-4"><Localize str="STR_Header" /></h1>
+            <div className="mb-4"></div>
+            <Row className="justify-content-center">
+                <Col>
+                    <Card className="bg-skycave " > 
+                        <Card.Header as="h3">Title</Card.Header>
+                        <Card.Body>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card className="bg-skycave">
+                        <Card.Header as="h3">Title</Card.Header>
+                        <Card.Body>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card className="bg-skycave">
+                        <Card.Header as="h3">Title</Card.Header>
+                        <Card.Body>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
         </Container>
     );
 }
